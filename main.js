a1581f2ec81913f5cbd5209738c7d59e05393fb5
Antonio Pons <toninopons@gmail.com>
2023-01-27 12:45:32 +0100

    Error handling added for invalid moves.

--- main.js
+++ main.js
@@ -9,32 +9,38 @@ function sleep(ms) {
 game.printToConsole();
 
 let userMove;
-
 const main = async () => {
+    let counter = 0;
+    
+    
     while(userMove !== 'EXIT') {
-        userMove = prompt('Enter your move:').toUpperCase();
-        //console.log(userMove.split(' '));
-        const [from, to] = userMove.split(' ');
-        game.move(from, to);
-        game.printToConsole();
-        await sleep(1000);
-        console.clear();
-        game.aiMove();
-        game.printToConsole();
+        //In the future instead of using the counter, this is better to extract game state from 
+        // game.getHistory()[i].configuration object.
+        
+        //console.log(game.getHistory());
+        let isWhitesTurn = counter % 2 === 0;
+        if (isWhitesTurn) {
+            userMove = prompt('Enter your move: ').toUpperCase();
+            const [from, to] = userMove.split(' ');
+            
+            try {
+                game.move(from, to);
+                game.printToConsole();
+            } catch {
+                game.printToConsole();
+                console.log('Invalid move!');
+                --counter;
+            }
+            
+            await sleep(1000);
+        } else {
+            const aiMoved = game.aiMove();
+            game.printToConsole();            
+        }
+
+
+        counter++;
     }
 }
 
 main();
-
-/*
-try {
-    game.move("G1", "F3");
-} catch(e) {
-    console.error(`${e}`);
-}
-
-let move = game.aiMove();
-console.log(move);
-game.printToConsole();
-//console.clear();
-*/
